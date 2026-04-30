7ace4419da11b163f78e4f2a69806e85323350a3
Bozhidar <bobicloudvision@gmail.com>
2026-04-30 10:46:18 +0300

    init

---
+++ js_juce/js_juce.h
@@ -0,0 +1,31 @@
+/*
+  ==============================================================================
+
+    This file is part of the js_juce module.
+
+  ==============================================================================
+*/
+
+#pragma once
+
+/*
+    BEGIN_JUCE_MODULE_DECLARATION
+
+    ID:                 js_juce
+    vendor:             js_juce
+    version:            0.0.1
+    name:               js_juce
+    description:        JavaScript-oriented helpers for JUCE projects.
+    website:            https://github.com
+    license:            MIT
+    dependencies:       juce_core
+
+    END_JUCE_MODULE_DECLARATION
+*/
+
+#include <juce_core/juce_core.h>
+
+namespace js_juce
+{
+inline constexpr const char* moduleName = "js_juce";
+}
